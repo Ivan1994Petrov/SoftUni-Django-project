a0bc49a79446010a8ee897846b409b6fb9c199ea
Ivan1994Petrov <ivan94hristov@gmail.com>
2019-05-03 14:21:26 +0300

    creating users

---
+++ lost_animals/accounts/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.views import generic
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+# Create your views here.
+
+def redirect_to_user_profile(request):
+    url = f'/accounts/profile/{request.user.id}/'
+    return HttpResponseRedirect(redirect_to=url)
+
+class UserProfileDetails(generic.DetailView):
+    model = User
+    template_name = 'user_profile.html'
+    context_object_name = 'user'
+
+
+class SignUp(generic.CreateView):
+    form_class = UserCreationForm
+    template_name = 'signup.html'
+    success_url = '/accounts/login/'
