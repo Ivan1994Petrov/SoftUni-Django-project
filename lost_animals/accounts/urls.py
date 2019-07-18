154a36d79d8a82d15784f72aa1234831bb87bd7a
Ivan1994Petrov <30923918+Ivan1994Petrov@users.noreply.github.com>
2019-07-18 15:18:40 +0300

    Update urls.py

--- lost_animals/accounts/urls.py
+++ lost_animals/accounts/urls.py
@@ -1,14 +1,4 @@
-# from django.urls import path, re_path, include
-# from . import views
-#
-#
-# urlpatterns = [
-#     path('', include('django.contrib.auth.urls')),
-#     re_path('^profile/(?P<pk>\d+)/$', views.UserProfileDetails.as_view(), name='user-details'),
-#     path('profile/', views.redirect_to_user_profile, name='user-redirect'),
-#     path('signup/', views.SignUp.as_view(), name='signup')
-#
-# ]
+
 from django.urls import path, re_path, include
 
 from . import views
